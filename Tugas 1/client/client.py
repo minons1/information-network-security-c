38c4703e9d3f53daffefcd54070cbfad36355849
minons1 <salimbinusman1@gmail.com>
2021-10-12 01:01:02 +0700

    using object serialization

--- Tugas 1/client/client.py
+++ Tugas 1/client/client.py
@@ -20,27 +20,40 @@ try:
         filename = message_split[1].rstrip("\n")
 
         # mengirimkan request filename ke server
-        cmd = msg('unduh', filename=filename)
-        client_socket.send(pickle.dump(cmd))
-        recv_data = client_socket.recv(2048)
-        res = pickle.loads(recv_data)
+        cmd = msg(text='unduh', filename=filename)
+        client_socket.sendall(pickle.dumps(cmd))
         
+        res = b''
+        while True:
+            recv_data = client_socket.recv(2048)
+            res += recv_data
+            print(":sekali")
+            if len(recv_data) < 2048 - 1:
+                print("break")
+                break
+        
+        data = pickle.loads(res)
+        print(type(data.key))
+        print("Received message: " + data.message 
+        + "\nfile name : " + data.filename 
+        + "\nfilesize: " + data.filesize
+        + "\niv:  " + str(data.iv))
         # membuat file dan mengisi data kedalam file
-        with open(filename, 'wb') as file:
-            # Panjang pesan untuk membatasi loop recv sesuai ukuran file
-            msg_len = message_header[1].decode('utf-8').split(" ")
-            file_len = (int(msg_len[1][:-1])//1024) + 1
-            print ('File dibuat')
+        # with open(filename, 'wb') as file:
+        #     # Panjang pesan untuk membatasi loop recv sesuai ukuran file
+        #     msg_len = message_header[1].decode('utf-8').split(" ")
+        #     file_len = (int(msg_len[1][:-1])//1024) + 1
+        #     print ('File dibuat')
 
-            # mengirimkan file yang masuk ke recv_data (untuk memenuhi slot 1024 bytes sebelum
-            # masuk ke variabel selanjutnya (data))
-            file.write(message_header[4])
+        #     # mengirimkan file yang masuk ke recv_data (untuk memenuhi slot 1024 bytes sebelum
+        #     # masuk ke variabel selanjutnya (data))
+        #     file.write(message_header[4])
             
-            while file_len:
-                print('Menerima data...')
-                data = client_socket.recv(1024)
-                file.write(data)
-                file_len-=1
+        #     while file_len:
+        #         print('Menerima data...')
+        #         data = client_socket.recv(1024)
+        #         file.write(data)
+        #         file_len-=1
 
         ## INTERRUPT DENGAN KEYBOARD UNTUK MENGHENTIKAN PROSES
 
