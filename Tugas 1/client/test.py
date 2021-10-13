b64daf62b8fb0abda83a031d5015a2265c867384
minons1 <salimbinusman1@gmail.com>
2021-10-13 11:49:15 +0700

    delete test file

--- Tugas 1/client/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-filename="Soal.png"
-try:
-    with open("dataset/"+filename, "rb") as file:
-        print(str(os.path.getsize("dataset/"+filename)))
-    
-except:
-    print("error")  
